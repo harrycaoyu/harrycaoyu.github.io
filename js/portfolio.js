7f445be97095ffbe484ce880a11dab93a64576a2
Dmitry Blotsky <dmitry.blotsky@gmail.com>
2014-05-26 20:19:50 -0400

    Made portfolio animate on scrollover. Added jquery.waypoint plugin. Did new contact section. Added custom fonts. Made header 'brizi' logo an image.

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,4 +1,9 @@
 $(function() {
     var height = Math.min(document.body.clientHeight, document.body.clientWidth) * 0.8;
     $("#bubble-container").height(height).width(height);
+    $(".portfolio").waypoint(function() {
+        $(".gps_ring").addClass("ring_animate");
+        $(".need_fast").addClass("fast").removeClass(".need_fast");
+        $(".need_fast").addClass("slow").removeClass(".need_slow");
+    }, { offset: 200 });
 });
